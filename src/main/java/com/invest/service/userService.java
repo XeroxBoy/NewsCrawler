227a40be5a1f41f682153d85387e4aacba34bbab
XeroxBoy <827312773@qq.com>
2017-10-26 20:31:19 +0800

    Default

--- src/main/java/com/invest/service/userService.java
+++ src/main/java/com/invest/service/userService.java
@@ -12,40 +12,20 @@ import javax.annotation.Resource;
 public class userService {
     @Resource
     private userDao userdao;
-
-
     public void insertUser(User user) {
-    /*SqlSession session=sqlSessionFactory.openSession();
-    userdao=session.getMapper(userDao.class);*/
         userdao.insertUser(user);
-	/*session.commit();
-	session.close();*/
     }
 
     public void deleteUser(String username) {
-/*	SqlSession session=sqlSessionFactory.openSession();
-	userdao=session.getMapper(userDao.class);*/
         userdao.deleteUser(username);
-/*	session.commit();
-    session.close();*/
     }
 
     public void updateUser(User user) {
-	/*SqlSession session=sqlSessionFactory.openSession();
-	userdao=session.getMapper(userDao.class);*/
         userdao.updateUser(user);
-	/*session.commit();
-	session.close();*/
     }
 
     public User selectUser(String username) {
-//userService userService=(com.invest.service.userService) ctx.getBean("userService");
-/*	SqlSession session=sqlSessionFactory.openSession();
-	userdao=session.getMapper(userDao.class);*/
         User user = userdao.selectUser(username);
-/*
-	session.close();
-*/
         return user;
     }
 }

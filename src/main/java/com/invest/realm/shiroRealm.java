6b0310ef2eb906e5848f1325b745ddaa43d991a6
XeroxBoy <827312773@qq.com>
2018-04-27 21:21:28 +0800

    Default

---
+++ src/main/java/com/invest/realm/shiroRealm.java
@@ -0,0 +1,45 @@
+package com.invest.realm;
+
+import com.invest.pojo.User;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.invest.service.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class shiroRealm extends AuthorizingRealm{
+    @Autowired
+    private userService userService;
+    //登陆成功的
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        String username= (String) principalCollection.getPrimaryPrincipal();
+        SimpleAuthorizationInfo info= new SimpleAuthorizationInfo();
+        Set<String> roles=new HashSet<>();
+        roles.add("user");
+        info.setRoles(roles);
+    info.setStringPermissions(roles);
+        return info;
+    }
+    //验证当前登录用户
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        String username=(String) authenticationToken.getPrincipal();
+        User user = userService.selectUser(username);
+        if(user!=null)
+        {
+            AuthenticationInfo info=new SimpleAuthenticationInfo(user.getUsername(),user.getPassword(),"realm");
+            return info;
+        }
+        else{return null;}
+    }
+}
+
---
+++ src/main/webapp/views/unauth.jsp
@@ -0,0 +1,7 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<body>
+失败
+</body>
+
+</html>
